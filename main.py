ce028326942e32f2fa2f34a3925460797fe1830b
abhis <abhishekshukla824@gmail.com>
2022-07-10 10:30:58 +0530

    # main scope or global scope, function calling, variable and input output on console

---
+++ main.py
@@ -0,0 +1,12 @@
+# main scope or global scope, function calling, variable and input output on console
+def fun():
+    print("enter first no")
+    x = int(input())
+    print("Print second number")
+    y = int(input())
+    print("Sum of two number is ")
+    print(x+y)
+
+
+if __name__ == "__main__":
+    fun()
