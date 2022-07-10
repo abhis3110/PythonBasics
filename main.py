69449229b2cb3670d83d9660cbd49534329e5bf2
abhis <abhishekshukla824@gmail.com>
2022-07-10 14:31:42 +0530

    Variables, types of variables and typecasting

--- main.py
+++ main.py
@@ -5,8 +5,21 @@ def fun():
     print("Print second number")
     y = int(input())
     print("Sum of two number is ")
-    print(x+y)
+    print(x + y)
+
+
+def var_types():
+    x = 3
+    print(type(x))
+    y = 3.7
+    print(type(y))
+    s = "Abhishek"
+    print(type(s))
+    si = "3"
+    i = int(si)
+    print(i)
 
 
 if __name__ == "__main__":
-    fun()
+    # fun()
+    var_types()
